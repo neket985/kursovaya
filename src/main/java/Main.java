6a08ebd2b2d5b6cd70b9001ad0d41554fd42b66d
nikitos <neket98@bk.ru>
2016-12-17 13:03:40 +0300

    17.12.16(2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,56 +19,50 @@ public class Main {
         final HashMap<Integer, Person> workers = new HashMap<Integer, Person>();
         final LinkedList<Person> workersShown = new LinkedList<Person>();
         JFrame swg = new JFrame("workers");
-        JButton add = new JButton("Добавить");
-        JButton remove = new JButton("Удалить");
+        JButton addBTN = new JButton("Добавить");
+        JButton removeBTN = new JButton("Удалить");
         JButton filterBTN = new JButton("Фильтр");
-        final JComboBox list = new JComboBox(listComboBox);
+        final JComboBox comboBox = new JComboBox(listComboBox);
+
         final JTextField filter = new JTextField();
         filter.setPreferredSize(new Dimension(200, 20));
-        list.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-
-                System.out.println(listComboBox[list.getSelectedIndex()]);
-            }
-        });
-        swg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        swg.setSize(600, 400);
 
         TableModel model = new MyTableModel(workersShown);
         final JTable table = new JTable(model);
         table.setGridColor(Color.BLACK);
 
         filter.addKeyListener(new KeyListener() {
-            public void keyTyped(KeyEvent e) {
-            }
-
-            public void keyPressed(KeyEvent e) {
-            }
-
+            public void keyTyped(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {}
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyChar() == '\n') {
-                    filterFun(workers, workersShown, list, filter, table);
+                    filterFun(workers, workersShown, comboBox, filter, table);
                 }
             }
         });
+
         filterBTN.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                filterFun(workers, workersShown, list, filter, table);
+                filterFun(workers, workersShown, comboBox, filter, table);
             }
         });
-        add.addActionListener(new AddListener(workers, workersShown, table));
-        remove.addActionListener(new RemoveListener(workers, workersShown, table));
+
+        addBTN.addActionListener(new AddListener(workers, workersShown, table));
+        removeBTN.addActionListener(new RemoveListener(workers, workersShown, table));
 
         JPanel pannel = new JPanel();
         pannel.setBackground(new Color(117, 181, 255));
         pannel.add(new JLabel("Фильтр"));
-        pannel.add(list);
-        pannel.add(remove);
-        pannel.add(add);
+        pannel.add(comboBox);
+        pannel.add(removeBTN);
+        pannel.add(addBTN);
         pannel.add(filter);
         pannel.add(filterBTN);
         pannel.add(table.getTableHeader());
         pannel.add(table);
+
+        swg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        swg.setSize(600, 400);
         swg.add(pannel);
         swg.setVisible(true);
     }
